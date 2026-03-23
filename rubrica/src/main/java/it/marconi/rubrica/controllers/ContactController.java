d98ad1a0a292a7c780641bec416f26141f74d91b
Emmanuele Grassi <emmanuele.grassi@marconirovereto.it>
2026-03-23 15:02:03 +0000

    feat: gestito il redirect dopo l'inserimento di un nuovo contatto

--- rubrica/src/main/java/it/marconi/rubrica/controllers/ContactController.java
+++ rubrica/src/main/java/it/marconi/rubrica/controllers/ContactController.java
@@ -1,10 +1,17 @@
 package it.marconi.rubrica.controllers;
 
+import java.util.UUID;
+
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.ModelAndView;
 
 import it.marconi.rubrica.domain.Contact;
@@ -19,7 +26,8 @@ public class ContactController {
 
     @GetMapping
     public ModelAndView showContactList(){
-        return new ModelAndView("contact-list");
+        //passo alla webpage  la lista dei contatti letta dal db
+        return new ModelAndView("contact-list").addObject("contacts", contactService.findAll());
     }
 
     @GetMapping("/new")
@@ -31,6 +39,19 @@ public class ContactController {
     @PostMapping("/new")
     public ModelAndView handleNewContact(@ModelAttribute ContactForm contactForm){
         Contact c =contactService.save(contactForm);
-        return new ModelAndView("contact-form");
+        // return new ModelAndView("contact-form");
+        return new ModelAndView("redirect:/contact?id="+c.getId());
+    }
+
+    @GetMapping(path="contact", params="id")
+    public ModelAndView showContact(@RequestParam("id") UUID contactID){
+        
+        Optional<Contact> opContact = contactService.get(contactID);
+        //controllo se il dato è presente
+        if (opContact.isPresent()){
+            return new ModelAndView("contact-detail").addObject(opContact.get());
+        }else{
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Contatto non trovato");
+        }   
     }
 }
